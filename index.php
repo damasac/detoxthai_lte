297536a7cbb328b960f7d3116ef8ecfb73816e27
ppanuwat <panuwat@thaimedresnet.org>
2015-06-02 21:09:45 +0700

    เพิ่ม CMS

---
+++ index.php
@@ -0,0 +1,122 @@
+<?php require_once '_theme/util.inc.php'; $MasterPage = 'page_main.php';?>
+
+<?php sb('title');?> Liver flushing registry <?php eb();?>
+
+<?php sb('js_and_css_head'); ?>
+<script src=""></script>
+<?php eb();?>
+
+<?php sb('content');?>
+<?php include_once "_connection/db.php"; ?>
+
+<!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Timeline (Feeds)
+      <small>จากทุก Sites</small>
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Detox Thai</li>
+    </ol>
+  </section>
+
+  <!-- Main content -->
+        <section class="content">
+        <div class="box box-primary">
+          <!-- row -->
+          <div class="row" style="margin: 10px;">
+            <div class="col-md-12">
+              <!-- The time line -->
+              <ul class="timeline">
+                <!-- timeline time label -->
+                <li class="time-label">
+                  <span class="bg-red">
+                    10 Feb. 2014
+                  </span>
+                </li>
+                <!-- /.timeline-label -->
+                <!-- timeline item -->
+                <li>
+                  <i class="fa fa-envelope bg-blue"></i>
+                  <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+                    <h3 class="timeline-header"><a href="#">Support Team</a> @ <a href="#">ธัญสมุย</a> ได้เพิ่ม <a href="#">กำหนดการหลักสูตรล้างพิษตับสำหรับผู้เริ่มต้น</a></h3>
+                    <div class="timeline-body">
+                      Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
+                      weebly ning heekya handango imeem plugg dopplr jibjab, movity
+                      jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
+                      quora plaxo ideeli hulu weebly balihoo...
+                    </div>
+                    <div class='timeline-footer'>
+                      <a class="btn btn-primary btn-xs">Read more</a>
+                      <a class="btn btn-danger btn-xs">Delete</a>
+                    </div>
+                  </div>
+                </li>
+                <!-- END timeline item -->
+                <!-- timeline item -->
+                <li>
+                  <i class="fa fa-user bg-aqua"></i>
+                  <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
+                    <h3 class="timeline-header no-border"><a href="#">Sarah Young</a> ได้สมัครเข้าเป็นสมาชิกใหม่</h3>
+                  </div>
+                </li>
+                <!-- END timeline item -->
+                <!-- timeline item -->
+                <li>
+                  <i class="fa fa-comments bg-yellow"></i>
+                  <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
+                    <h3 class="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
+                    <div class="timeline-body">
+                      Take me to your leader!
+                      Switzerland is small and neutral!
+                      We are more like Germany, ambitious and misunderstood!
+                    </div>
+                    <div class='timeline-footer'>
+                      <a class="btn btn-warning btn-flat btn-xs">View comment</a>
+                    </div>
+                  </div>
+                </li>
+                <!-- END timeline item -->
+                <!-- timeline time label -->
+                <li class="time-label">
+                  <span class="bg-green">
+                    3 Jan. 2014
+                  </span>
+                </li>
+                <!-- /.timeline-label -->
+                <!-- timeline item -->
+                <li>
+                  <i class="fa fa-camera bg-purple"></i>
+                  <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
+                    <h3 class="timeline-header"><a href="#">Mina Lee</a> @ <a href="#">บ้านสุขภาพเขาใหญ่</a> uploaded new photos</h3>
+                    <div class="timeline-body">
+                      <img src="http://placehold.it/150x100" alt="..." class='margin' />
+                      <img src="http://placehold.it/150x100" alt="..." class='margin'/>
+                      <img src="http://placehold.it/150x100" alt="..." class='margin'/>
+                      <img src="http://placehold.it/150x100" alt="..." class='margin'/>
+                    </div>
+                  </div>
+                </li>
+                <!-- END timeline item -->
+                <li>
+                  <i class="fa fa-clock-o bg-gray"></i>
+                </li>
+              </ul>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- / .box solid -->
+
+        </section><!-- /.content -->
+  
+<?php eb();?>
+
+
+<?php sb('js_and_css_footer');?>
+<?php eb();?>
+ 
+<?php render($MasterPage);?>
