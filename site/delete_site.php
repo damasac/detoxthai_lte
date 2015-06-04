d5d608cc6b29b8d36386f6fece162192a3cf5146
codeerror <8ohkoovd@gmail.com>
2015-06-04 14:22:08 +0700

    Fixed.

--- site/delete_site.php
+++ site/delete_site.php
@@ -13,8 +13,13 @@
     //$result->execute();
     //$row = $result->fetch(PDO::FETCH_ASSOC);
 
+    $result = $mysqli->query("SELECT site_url
+                            FROM site_detail WHERE id = '$site_id'");
+    $row = $result->fetch_assoc();
+
     if($result->num_rows > 0){
         $result = $mysqli->query("DELETE FROM site_detail WHERE id = '$site_id'");
+        $result = $mysqli->query("DELETE FROM site_menu WHERE site_name = '".$row['site_url']."'");
         //$result->execute();
         //$host  = $_SERVER['HTTP_HOST'];
         header('location: site_manage.php');
--- site/index.php
+++ site/index.php
@@ -3,44 +3,198 @@
 <?php sb('title');?> Liver flushing registry <?php eb();?>
 
 <?php sb('js_and_css_head'); ?>
-<script src=""></script>
+  <script type="text/javascript" src="../_plugins/jQuery/jQuery-2.1.4.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="../_plugins/edit/minified/themes/default.min.css">
+  <script src="../_plugins/edit/minified/jquery.sceditor.bbcode.min.js"></script>
+  <style type="text/css">
+    .sceditor-container {
+      margin-left: 17px;
+    }
+  </style>
 <?php eb();?>
 
 <?php sb('content');?>
 <?php include_once "../_connection/db.php"; ?>
 
+<?php
+$site_name = $_GET['site_name'];
+$menu = $_GET['menu'];
+
+$edit_show = 1;
+?>
+
 <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <h1>
-      จัดการหน้าเว็บ
-      <small>แก้ไขเนื้อหาหรือเมนูของเว็บ</small>
-    </h1>
-    <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> หน้าหลัก</a></li>
+<section class="content-header">
+  <h1>
+    จัดการหน้าเว็บ
+    <small>แก้ไขเนื้อหาหรือเมนูของเว็บ</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> หน้าหลัก</a></li>
     <li><a href="#">ค่ายล้างพิษตับ</a></li>
-     <li><a href="#">จัดการศูนย์</a></li>
-    <li class="active">จัดการหน้าเว็บ </li>
-    </ol>
-  </section>
+    <li><a href="#">จัดการศูนย์</a></li>
+    <li class="active">จัดการหน้าเว็บ</li>
+  </ol>
+</section>
 
-  <!-- Main content -->
-  <section class="content">
+<!-- Main content -->
+<section class="content">
 
-    <div class="box box-default">
-        <div class="box-header with-border">
-          <h3 class="box-title">Blank Box</h3>
+  <div class="box box-default">
+    <div class="box-body">
+      <div class="row">
+        <div class="col-md-3">
+          <h3 class="text-muted">ธัญญสมุย</h3>
         </div>
-        <div class="box-body">
-          The great content goes here
-        </div><!-- /.box-body -->
-  </div><!-- /.box -->
+        <div class="col-md-9">
+          <nav>
+            <ul class="nav nav-pills pull-right">
+              <?php
+              $result = $mysqli->query("SELECT id, menu_name, display_menu FROM site_menu WHERE site_name = '$site_name' ORDER BY id");
+              //$result->execute();
 
-  </section><!-- /.content -->'
+              if ($result !== false) {
+                foreach($result as $row) {
+
+                  $menu_sub_show = 0;
+                  $html_sub_menu = "";
+                  $getSubMenu = $mysqli->query("SELECT menu_name FROM site_submenu WHERE main_menu_id = ".$row['id']." ORDER BY id");
+                  //$getSubMenu->execute();
+                  if ($getSubMenu !== false) {
+                    $count = $getSubMenu->num_rows;
+                    if(0 < $count){
+                      $menu_sub_show = 1;
+                    }
+                    foreach($getSubMenu as $submenu) {
+                      $html_sub_menu .= "<li role='presentation'><a role='menuitem' tabindex='-1' href='index.php?menu=".trim($submenu['menu_name'])."&site_name=".$site_name."&sub_menu=1'>".$submenu['menu_name']."</a></li>";
+                    }
+                  }
+                  if(0 == $row['display_menu'] && $menu_sub_show == 0){
+                    if(strcmp($menu, $row['menu_name']) === 0){
+                      echo "<li role='presentation' class='active'><a href='index.php?menu=".trim($row['menu_name'])."&site_name=".$site_name."'>".trim($row['menu_name'])."</a></li>";
+                    }else{
+                      echo "<li role='presentation'><a href='index.php?menu=".trim($row['menu_name'])."&site_name=".$site_name."'>".trim($row['menu_name'])."</a></li>";
+                    }
+                  }else{
+                    echo "<li class='dropdown'>
+                    <a id='drop1' href='#' class='dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' role='button' aria-expanded='false'>
+                      ".$row['menu_name']."
+                      <span class='caret'></span>
+                    </a>
+                    <ul class='dropdown-menu' role='menu' aria-labelledby='drop1'>
+                      ".$html_sub_menu."
+                    </ul>
+                  </li>";
+                }
+              }
+            }
+            ?>
+
+            <?php if(0 !== $edit_show){ ?>
+            <li class="dropdown">
+              <a id="drop1" href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" role="button" aria-expanded="false">
+                ผู้ดูแลระบบ
+                <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="menu.php?site_name=<?php echo $site_name; ?>">แก้ไขเมนู</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="site_schedule.php?site_url=<?php echo $site_name; ?>">กำหนดการหลักสูตร</a></li>
+              </ul>
+            </li>
+            <?php } ?>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+    <div class="row marketing" id="show_content">
+          <?php
+            $sub_menu = 0;
+            isset($_GET['sub_menu'])? $sub_menu = 1 : $sub_menu = 0;
+            if(1 != $sub_menu){
+              $result = $mysqli->query("SELECT menu_name, display_menu, content_id, content_html
+                                      FROM site_menu
+                                      INNER JOIN site_content ON site_menu.content_id = site_content.id
+                                      WHERE menu_name = '$menu' AND
+                                      site_name = '$site_name'
+                                      ORDER BY site_menu.id");
+                    //$result->execute();
+                    $row = $result->fetch_assoc();
+            }else{
+              $result = $mysqli->query("SELECT menu_name, status_menu, content_id, content_html
+                                      FROM site_submenu
+                                      INNER JOIN site_content ON site_submenu.content_id = site_content.id
+                                      WHERE menu_name = '$menu' AND
+                                      site_name = '$site_name'
+                                      ORDER BY site_submenu.id");
+                    //$result->execute();
+                    $row = $result->fetch_assoc();
+            }
+          ?>
+          <textarea name="textarea" rows="50"><?php echo $row['content_html']; ?></textarea>
+          <div id="fix">
+          <?php
+            echo "<textarea name='textarea' rows='0' style='display: none;'></textarea>";
+            echo "<script>
+                  $('textarea').sceditor({
+                      plugins: 'bbcode',
+                      width: '95%',
+                      resizeEnabled: false,
+                      style: 'edit/minified/jquery.sceditor.default.min.css'
+                  });
+                  var html = $('textarea').sceditor('instance').fromBBCode('".trim(preg_replace('/\s+/', ' ', $row['content_html']))."');
+                  //alert(html);
+                  $('#show_content').html(html);
+                  $('.sceditor-container').hide();
+                  </script>";
+          ?>
+          </div>
+      </div>
+  </div>
+
+</div><!-- /.box-body -->
+</div><!-- /.box -->
+
+</section><!-- /.content -->'
 
 <?php eb();?>
 
 
 <?php sb('js_and_css_footer');?>
+  <script>
+        $(function() {
+            // Replace all textarea's
+            // with SCEditor
+            $("textarea").sceditor({
+                plugins: "bbcode",
+                width: '80%',
+                resizeEnabled: false,
+                style: "edit/minified/jquery.sceditor.default.min.css"
+            });
+
+            $( "#save" ).click(function() {
+              var bbCode = $( 'textarea' ).sceditor( 'instance' ).val();
+              //var html = $( "textarea" ).sceditor( 'instance' ).fromBBCode(bbCode);
+              //alert( bbCode );
+              $.post("edit_content.php",
+                {
+                  id: <?php echo $row['content_id']; ?>,
+                  content_html: bbCode,
+                  token: $('#token').val(),
+                },
+                function(data,status){
+                    //alert("Data: " + data + "\nStatus: " + status);
+                    location.reload();
+                });
+            });
+
+            //$('#show_content').html('cc');
+
+            });
+
+            $('#fix').hide();
+            //alert('Codeerror');
+      </script>
 <?php eb();?>
 
 <?php render($MasterPage);?>
