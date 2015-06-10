14e93181db89ad45bbc7abd3596aec3c16bb938f
codeerror <8ohkoovd@gmail.com>
2015-06-10 14:05:11 +0700

    Fixed.

--- login.php
+++ login.php
@@ -195,34 +195,25 @@ if ($user) {
             <div class="col-xs-8">
               <div class="checkbox icheck">
                 <label>
-                  <input type="checkbox"> Remember Me
+                  <input type="checkbox"> จดจำฉันไว้ในคราวต่อไป
                 </label>
               </div>
             </div><!-- /.col -->
             <div class="col-xs-4">
 	      <input type="hidden" name="returnurl" value="<?php echo $_SERVER['HTTP_REFERER'];?>">
-              <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+              <button type="submit" class="btn btn-primary btn-block btn-flat">เข้าใช้งาน</button>
             </div><!-- /.col -->
           </div>
         </form>
 
         <div class="social-auth-links text-center">
-<<<<<<< Updated upstream
-          <p>- OR -</p>
-          <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
-          <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
-=======
           <p>- หรือ -</p>
-          <a class="btn btn-block btn-social btn-facebook btn-flat" scope="public_profile,email" onclick="checkLoginState();"><i class="fa fa-facebook"></i> เข้าใช้งานโดยใช้ เฟซบุ๊ค</a>
+          <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> เข้าใช้งานโดยใช้ เฟซบุ๊ค</a>
           <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> เข้าใช้งานโดยใช้  Google+</a>
-	  <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-	  </fb:login-button>
-	  <div id="status"></div>
->>>>>>> Stashed changes
         </div><!-- /.social-auth-links -->
 
-        <a href="#">I forgot my password</a><br>
-        <a href="register.html" class="text-center">Register a new membership</a>
+        <a href="#">ลืมรหัสผ่าน</a><br>
+        <a href="register.html" class="text-center">สมัครสมาชิกใหม่</a>
 
       </div><!-- /.login-box-body -->
     </div><!-- /.login-box -->
