4f853c6dfb1ebef7c3c740bea280979ca3085bd4
DAMASAC KKU <damasacdev@gmail.com>
2015-06-12 16:59:50 +0700

    db_base

--- _connection/db_base.php
+++ _connection/db_base.php
@@ -1,7 +1,7 @@
 <?php
 
 ## System Start ############################################################
-$mysqli = new mysqli("localhost", "root", "", "detoxthai");
+$mysqli = new mysqli("localhost", "root", "", "detoxthai_lte");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
