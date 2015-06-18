a9f8232700df5db2fe73acc7bf1e5298cafac5c1
ball69 <skaball69@gmail.com>
2015-06-18 15:06:54 +0700

    Fixed นามแฝงหน้าไทมไลน์

--- timeline/index.php
+++ timeline/index.php
@@ -9,13 +9,8 @@ $MasterPage = 'page_main.php';?>
 <?php eb();?>
 
 <?php sb('js_and_css_head'); ?>
-<link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
-<!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
-<link rel="stylesheet" href="blueimp/css/jquery.fileupload.css">
-<link rel="stylesheet" href="blueimp/css/jquery.fileupload-ui.css">
-<!-- CSS adjustments for browsers with JavaScript disabled -->
-<noscript><link rel="stylesheet" href="blueimp/css/jquery.fileupload-noscript.css"></noscript>
-<noscript><link rel="stylesheet" href="blueimp/css/jquery.fileupload-ui-noscript.css"></noscript>
+        <link href="../_plugins/js-fileinput/css/fileinput.css" media="all" rel="stylesheet" type="text/css" />
+        <script src="../_plugins/js-fileinput/js/fileinput.min.js" type="text/javascript"></script>
 <?php eb();?>
 
 <?php sb('content');?>
@@ -78,24 +73,56 @@ $MasterPage = 'page_main.php';?>
   <!-- Main content -->
         <section class="content">
             <div class="box box-primary">
+
                 <br>
                 <div class="container">
-<!--                    <div class="row">
+                    <div class="row">
                         <div class="col-md-2">
                             <label>คุณกำลังคิดอะไรอยู่</label><br>
                             <code id="valPost" style="display:none;"></code>
                         </div>
+<form id="post" action="sql.php" enctype="multipart/form-data">
                         <div class="col-md-8">
-                                    <input type="text" class="form-control" id="timeline_post" name="timeline_post"><br>
-                                    <button class="btn btn-primary btn-flat margin"
-                                    data-toggle="modal" data-target=".bs-example-modal-lg"><i class="fa fa-image"></i></button>
+            <input type="text" class="form-control" id="timeline_post" name="timeline_post"><br>
 
-    <tbody class="files"></tbody>
+                <input id="file" class="images" type="file" name="images[]"  accept="image/*"  multiple="multiple">
+                <br>
+                <button type="submit" class="btn btn-primary">โพสต์</button>
+</form>
+<script>
+            var $input = $("#file");
+                $('#file').fileinput({
+                        showUpload:false,
+                        uploadAsync: false,
+                        uploadUrl: "sql.php", // your upload server url
+                        
+                        uploadExtraData: function() {
+                            return {
+                                //userid: $("#userid").val(),
+                                //username: $("#username").val()
+                            };
+                        },
+                         initialPreview: [
+                                    "<img src='http://placeimg.com/200/150/nature/1'>",
+                                    "<img src='http://placeimg.com/200/150/nature/2'>",
+                                    "<img src='http://placeimg.com/200/150/nature/3'>",
+                        ],
+                        initialPreviewConfig: [
+                            {caption: "Food-1.jpg", width: "120px", url: "/site/file-delete", key: 1},
+                         
+                        ]
+            }).on("filebatchselected", function(event, files) {
+    // trigger upload method immediately after files are selected
+    $input.fileinput("upload");
+});
+                
+$("#file").on("filepresfdfdelete", function(jqXHR) {
+            alert("delete");
+});
+
+</script>
                         </div>
-                        <div class="col-md-2">
-                            <button class="btn btn-primary btn-flat" onclick="postStatus();">โพสต์</button>
-                        </div>
-                    </div>-->
+                    </div>
                 </div>
 
                 <br>
@@ -124,7 +151,13 @@ $MasterPage = 'page_main.php';?>
                   <i class="fa fa-user-plus bg-green"></i>
                   <div class="timeline-item">
                     <span class="time"><i class="fa fa-clock-o"></i> <?php echo TimeThai($dateCreate["createdate"]);?></span>
-                    <h3 class="timeline-header no-border"><a  ><?php echo $dateCreate["fname"];?> <?php echo $dateCreate["lname"];?></php></a> ได้สมัครเข้าเป็นสมาชิกใหม่</h3>
+                    <h3 class="timeline-header no-border"><a  >
+                    <?php if($dateCreate["nickname"]==""){?>
+                    <?php echo $dateCreate["fname"]." ".$dateCreate["lname"];?>
+                    <?php }else{?>
+                    <?php echo $dateCreate["nickname"];?>
+                    <?php }?>
+                    </php></a> ได้สมัครเข้าเป็นสมาชิกใหม่</h3>
                   </div>
                 </li>
                <?php }?>
@@ -179,68 +212,7 @@ $MasterPage = 'page_main.php';?>
         </div><!-- / .box solid -->
 
         </section><!-- /.content -->
-<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content" style="padding:10px;">
 
-    <!-- The file upload form used as target for the file upload widget -->
-    <form id="fileupload" action="//jquery-file-upload.appspot.com/" method="POST" enctype="multipart/form-data">
-        <!-- Redirect browsers with JavaScript disabled to the origin page -->
-        <noscript><input type="hidden" name="redirect" value="https://blueimp.github.io/jQuery-File-Upload/"></noscript>
-        <!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
-        <div class="row fileupload-buttonbar">
-            <div class="col-lg-7">
-                <!-- The fileinput-button span is used to style the file input field as button -->
-                <span class="btn btn-success fileinput-button">
-                    <i class="glyphicon glyphicon-plus"></i>
-                    <span>Add files...</span>
-                    <input type="file" name="files[]" multiple>
-                </span>
-                <button type="submit" class="btn btn-primary start">
-                    <i class="glyphicon glyphicon-upload"></i>
-                    <span>Start upload</span>
-                </button>
-                <button type="reset" class="btn btn-warning cancel">
-                    <i class="glyphicon glyphicon-ban-circle"></i>
-                    <span>Cancel upload</span>
-                </button>
-
-
-                <!-- The global file processing state -->
-                <span class="fileupload-process"></span>
-            </div>
-            <!-- The global progress state -->
-            <div class="col-lg-5 fileupload-progress fade">
-                <!-- The global progress bar -->
-                <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-                    <div class="progress-bar progress-bar-success" style="width:0%;"></div>
-                </div>
-                <!-- The extended global progress state -->
-                <div class="progress-extended">&nbsp;</div>
-            </div>
-        </div>
-        <!-- The table listing the files available for upload/download -->
-        <table role="presentation" class="table">
-            <tr>
-                <td>Preview</td>
-                <td>Name</td>
-                <!--<td>size</td>s-->
-                <td>
-                    <div class="fileupload-buttonbar">
-                    <button type="button" class="btn btn-danger delete">
-                            <i class="glyphicon glyphicon-trash"></i>
-                    </button>
-                    <input type="checkbox" class="toggle">
-                    </div>
-            </td>
-            </tr>
-            <tbody class="files"></tbody>
-        </table>
-    </form>
-
-    </div>
-  </div>
-</div>
 
 <?php eb();?>
 
@@ -277,162 +249,6 @@ $MasterPage = 'page_main.php';?>
 </script>
 
 
-<!-- The template to display files available for upload -->
-<script id="template-upload" type="text/x-tmpl">
-{% for (var i=0, file; file=o.files[i]; i++) { %}
-    <tr class="template-upload fade">
-        <td>
-            <span class="preview"></span>
-        </td>
-        <td>
-            <p class="name">{%=file.name%}</p>
-            <strong class="error text-danger"></strong>
-        </td>
-        <td width="350px">
-            <p class="size">Processing...</p>
-            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
-        </td>
-        <td style="float:left;">
-            {% if (!i && !o.options.autoUpload) { %}
-                <button class="btn btn-primary start" disabled>
-                    <i class="glyphicon glyphicon-upload"></i>
-                    <span>Start</span>
-                </button>
-            {% } %}
-            {% if (!i) { %}
-                <button class="btn btn-warning cancel">
-                    <i class="glyphicon glyphicon-ban-circle"></i>
-                    <span>Cancel</span>
-                </button>
-            {% } %}
-        </td>
-    </tr>
-{% } %}
-</script>
-<!-- The template to display files available for download -->
-<script id="template-download" type="text/x-tmpl">
-
-{% for (var i=0, file; file=o.files[i]; i++) { %}
-
-    <tr class="template-download fade">
-        <td>
-            <span class="preview">
-                {% if (file.thumbnailUrl) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
-                {% } %}
-            </span>
-        </td>
-        <td>
-            <p class="name">
-                {% if (file.url) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
-                {% } else { %}
-                    <span>{%=file.name%}</span>
-                {% } %}
-            </p>
-            {% if (file.error) { %}
-                <div><span class="label label-danger">Error</span> {%=file.error%}</div>
-            {% } %}
-        </td>
-        <!--<td>-->
-        <!--    <!--<span class="size">{%=o.formatFileSize(file.size)%}</span>-->-->
-        <!--</td>-->
-        <td>
-            {% if (file.deleteUrl) { %}
-                <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
-                    <i class="glyphicon glyphicon-trash"></i>
-
-                </button>
-                <input type="checkbox" name="delete" value="1" class="toggle">
-            {% } else { %}
-                <button class="btn btn-warning cancel">
-                    <i class="glyphicon glyphicon-ban-circle"></i>
-                    <span>Cancel</span>
-                </button>
-            {% } %}
-        </td>
-    </tr>
-{% } %}
-</script>
-<script id="template-download" type="text/x-tmpl">
-
-{% for (var i=0, file; file=o.files[i]; i++) { %}
-
-    <tr class="template-download fade">
-        <td>
-            <span class="preview">
-                {% if (file.thumbnailUrl) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
-                {% } %}
-            </span>
-        </td>
-        <td>
-            <p class="name">
-                {% if (file.url) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
-                {% } else { %}
-                    <span>{%=file.name%}</span>
-                {% } %}
-            </p>
-            {% if (file.error) { %}
-                <div><span class="label label-danger">Error</span> {%=file.error%}</div>
-            {% } %}
-        </td>
-        <!--<td>-->
-        <!--    <!--<span class="size">{%=o.formatFileSize(file.size)%}</span>-->-->
-        <!--</td>-->
-        <td>
-            {% if (file.deleteUrl) { %}
-                <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
-                    <i class="glyphicon glyphicon-trash"></i>
-
-                </button>
-                <input type="checkbox" name="delete" value="1" class="toggle">
-            {% } else { %}
-                <button class="btn btn-warning cancel">
-                    <i class="glyphicon glyphicon-ban-circle"></i>
-                    <span>Cancel</span>
-                </button>
-            {% } %}
-        </td>
-    </tr>
-{% } %}
-</script>
-<script src="blueimp/js/jquery.min.js"></script>
-<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> -->
-<!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
-<script src="blueimp/js/vendor/jquery.ui.widget.js"></script>
-<!-- The Templates plugin is included to render the upload/download listings -->
-<script src="blueimp/js/tmpl.min.js"></script>
-<!-- The Load Image plugin is included for the preview images and image resizing functionality -->
-<script src="blueimp/js/load-image.all.min.js"></script>
-<!-- The Canvas to Blob plugin is included for image resizing functionality -->
-<script src="blueimp/js/canvas-to-blob.min.js"></script>
-<!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
-<script src="blueimp/js/bootstrap.min.js"></script>
-<!-- blueimp Gallery script -->
-<!-- <script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script> -->
-<!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
-<script src="blueimp/js/jquery.iframe-transport.js"></script>
-<!-- The basic File Upload plugin -->
-<script src="blueimp/js/jquery.fileupload.js"></script>
-<!-- The File Upload processing plugin -->
-<script src="blueimp/js/jquery.fileupload-process.js"></script>
-<!-- The File Upload image preview & resize plugin -->
-<script src="blueimp/js/jquery.fileupload-image.js"></script>
-<!-- The File Upload audio preview plugin -->
-<script src="blueimp/js/jquery.fileupload-audio.js"></script>
-<!-- The File Upload video preview plugin -->
-<script src="blueimp/js/jquery.fileupload-video.js"></script>
-<!-- The File Upload validation plugin -->
-<script src="blueimp/js/jquery.fileupload-validate.js"></script>
-<!-- The File Upload user interface plugin -->
-<script src="blueimp/js/jquery.fileupload-ui.js"></script>
-<!-- The main application script -->
-<script src="blueimp/js/main.js"></script>
-<!-- The XDomainRequest Transport is included for cross-domain file deletion for IE 8 and IE 9 -->
-<!--[if (gte IE 8)&(lt IE 10)]>
-<script src="js/cors/jquery.xdr-transport.js"></script>
 <![endif]-->
 <?php eb();?>
 
