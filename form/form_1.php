17ddb0a0d4c596dd779564b10f8d1818edbf4873
kongvut sangkla <kongvut@kkumail.com>
2015-06-18 19:44:42 +0700

    update

--- form/form_1.php
+++ form/form_1.php
@@ -430,7 +430,7 @@
 	{
 	    var today = new Date();
 	    var birthDate = new Date(dateString);
-	    var age = today.getFullYear() - birthDate.getFullYear();
+	    var age = (today.getFullYear()+543) - birthDate.getFullYear();
 	    var m = today.getMonth() - birthDate.getMonth();
 	    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
 	    {
--- form/js/bootstrap-datepicker.js
+++ form/js/bootstrap-datepicker.js
@@ -208,7 +208,7 @@
 					html.push('<tr>');
 				}
 				clsName = this.onRender(prevMonth);
-				prevY = prevMonth.getFullYear()+543;
+				prevY = prevMonth.getFullYear();
 				prevM = prevMonth.getMonth();
 				if ((prevM < month &&  prevY === year) ||  prevY < year) {
 					clsName += ' old';
@@ -432,7 +432,7 @@
 				d: date.getDate(),
 				m: date.getMonth() + 1,
 				yy: date.getFullYear().toString().substring(2),
-				yyyy: date.getFullYear()
+				yyyy: date.getFullYear()+543
 			};
 			val.dd = (val.d < 10 ? '0' : '') + val.d;
 			val.mm = (val.m < 10 ? '0' : '') + val.m;
