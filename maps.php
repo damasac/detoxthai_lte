92fb6caacaedc6eb51956fd3d4a39ac0b80bb2aa
DAMASAC KKU <damasacdev@gmail.com>
2015-06-19 09:12:33 +0700

    เพิ่มเมนู

--- maps.php
+++ maps.php
@@ -14,8 +14,23 @@
         padding: 0px
       }
     </style>
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700|Open+Sans+Condensed:700,300,300italic|Open+Sans:400,300italic,400italic,600,600italic,700,700italic,800,800italic|PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css'/>
+    <link href="http://maps.marnoto.com/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="http://maps.marnoto.com/assets/css/bootstrap-responsive.css" rel="stylesheet">
+	<!--[if lt IE 9]><link rel="stylesheet" type="text/css" media="screen" href="../../assets/css/slide-in.ie.css" /><![endif]-->
+    <link href="http://maps.marnoto.com/assets/css/style.css" rel="stylesheet">
+    <!-- Color Style Setting CSS file-->
+    <link href="http://maps.marnoto.com/assets/css/color-theme/color-cgreen.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="http://maps.marnoto.com/en/5wayscustomizeinfowindow/css/style.css">
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
     <script>
+
 // The following example creates complex markers to indicate beaches near
 // Sydney, NSW, Australia. Note that the anchor is set to
 // (0,32) to correspond to the base of the flagpole.
@@ -86,14 +101,38 @@ function setMarkers(map, locations) {
   for (var i = 0; i < locations.length; i++) {
     var beach = locations[i];
     var myLatLng = new google.maps.LatLng(beach[1], beach[2]);
-    var marker = new google.maps.Marker({
+    var content = '<div id="iw-container">' +
+                      '<div class="iw-title">Porcelain Factory of Vista Alegre</div>' +
+                      '<div class="iw-content">' +
+                        '<div class="iw-subTitle">History</div>' +
+                        '<p>Founded in 1824, the Porcelain Factory of Vista Alegre was the first industrial unit dedicated to porcelain production in Portugal. For the foundation and success of this risky industrial development was crucial the spirit of persistence of its founder, José Ferreira Pinto Basto. Leading figure in Portuguese society of the nineteenth century farm owner, daring dealer, wisely incorporated the liberal ideas of the century, having become "the first example of free enterprise" in Portugal.</p>' +
+                        '<div class="iw-subTitle">Contacts</div>' +
+                        '<p>VISTA ALEGRE ATLANTIS, SA<br>3830-292 Ílhavo - Portugal<br>'+
+                        '<br>Phone. +351 234 320 600<br>e-mail: geral@vaa.pt<br>www: www.myvistaalegre.com</p>'+
+                      '</div>' +
+                      '<div class="iw-bottom-gradient"></div>' +
+                    '</div>';
+
+    var myinfowindow = new google.maps.InfoWindow({
+        content: content
+    });
+    
+    var markers = new google.maps.Marker({
         position: myLatLng,
         map: map,
         shape: shape,
         title: beach[0],
-        zIndex: beach[3]
+        zIndex: beach[3],
+        infowindow: myinfowindow
     });
+
+    google.maps.event.addListener(markers, 'click', function() {
+            this.infowindow.open(map, this);
+    
+    });
+
   }
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
