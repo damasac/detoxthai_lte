43e0ce217cd635df84fef429853a49893692580a
ball69 <skaball69@gmail.com>
2015-06-24 14:20:47 +0700

    Fixed

--- usermgn/index.php
+++ usermgn/index.php
@@ -80,14 +80,10 @@ $MasterPage = 'page_main.php';?>
                 <td><?php echo $data1["nickname"];?></td>
                 <?php }?>
                 <?php if($num2==1){?>
-                
+
                 <td><?php echo $data1["tel"];?></td>
                 <td>
-                <?php if($user_form==$data1["id"]){?>
-                    <button class="btn bg-maroon btn-flat margin"  onclick="backUser(<?php echo $session;?>)">
-                    <i class="fa fa-close"></i>
-                    Logout</button>
-                <?php }?>
+
                     <button class="btn bg-navy-active  color-palette btn-flat margin" onclick="goForm(<?php echo $data1["id"]; ?>);">
                     <i class="fa fa-edit "></i>
                     กรอกข้อมูล</button>
@@ -122,7 +118,7 @@ $MasterPage = 'page_main.php';?>
         function(data,status){
 
             location.reload();
-            
+
         });
     }
     function goForm(id) {
