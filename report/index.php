1373596df597abfae489750e385c5432b52b3b12
ghi krit <krit.ptd@gmail.com>
2015-06-27 22:36:38 +0700

    pie chart

--- report/index.php
+++ report/index.php
@@ -117,85 +117,8 @@ $MasterPage = 'page_main.php';?>
                       <p class="text-center">
                         <h3 class="box-title">ส่วนที่ 2: การล้างพิษตับ</h3>
                       </p>
-                          <script src="<?php echo 'http://',$_SERVER['SERVER_NAME'],'/',APP_WEBROOT;?>/_plugins/chartjs/Chart.min.js" type="text/javascript"></script>
-                          <script>
-                              //-------------
-                              //- PIE CHART -
-                              //-------------
-                              // Get context with jQuery - using jQuery's .get() method.
-                              var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
-                              var pieChart = new Chart(pieChartCanvas);
-                              var PieData = [
-                                {
-                                  value: 700,
-                                  color: "#f56954",
-                                  highlight: "#f56954",
-                                  label: "Chrome"
-                                },
-                                {
-                                  value: 500,
-                                  color: "#00a65a",
-                                  highlight: "#00a65a",
-                                  label: "IE"
-                                },
-                                {
-                                  value: 400,
-                                  color: "#f39c12",
-                                  highlight: "#f39c12",
-                                  label: "FireFox"
-                                },
-                                {
-                                  value: 600,
-                                  color: "#00c0ef",
-                                  highlight: "#00c0ef",
-                                  label: "Safari"
-                                },
-                                {
-                                  value: 300,
-                                  color: "#3c8dbc",
-                                  highlight: "#3c8dbc",
-                                  label: "Opera"
-                                },
-                                {
-                                  value: 100,
-                                  color: "#d2d6de",
-                                  highlight: "#d2d6de",
-                                  label: "Navigator"
-                                }
-                              ];
-                              var pieOptions = {
-                                //Boolean - Whether we should show a stroke on each segment
-                                segmentShowStroke: true,
-                                //String - The colour of each segment stroke
-                                segmentStrokeColor: "#fff",
-                                //Number - The width of each segment stroke
-                                segmentStrokeWidth: 1,
-                                //Number - The percentage of the chart that we cut out of the middle
-                                percentageInnerCutout: 50, // This is 0 for Pie charts
-                                //Number - Amount of animation steps
-                                animationSteps: 100,
-                                //String - Animation easing effect
-                                animationEasing: "easeOutBounce",
-                                //Boolean - Whether we animate the rotation of the Doughnut
-                                animateRotate: true,
-                                //Boolean - Whether we animate scaling the Doughnut from the centre
-                                animateScale: false,
-                                //Boolean - whether to make the chart responsive to window resizing
-                                responsive: true,
-                                // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-                                maintainAspectRatio: false,
-                                //String - A legend template
-                                legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
-                                //String - A tooltip template
-                                tooltipTemplate: "<%=value %> <%=label%> users"
-                              };
-                              //Create pie or douhnut chart
-                              // You can switch between pie and douhnut using the method below.  
-                              pieChart.Doughnut(PieData, pieOptions);
-                              //-----------------
-                              //- END PIE CHART -
-                              //-----------------                          
-                          </script>
+
+                   
                       <div class="box box-default">
                         <div class="box-header with-border">
                           <h3 class="box-title">กราฟวงกลมแสดงจำนวนผู้ล้างพิษตับ จำแนกตาม เพศ ระดับการศึกษา อาชีพ</h3>
@@ -239,7 +162,7 @@ $MasterPage = 'page_main.php';?>
                             <tr>
                               <td>1-10</td>
                               <td>
-                                <div class="progress progress-xs">
+                                <div class="progress progress-xs progress-striped active">
                                   <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
                                 </div>
                               </td>
@@ -248,7 +171,7 @@ $MasterPage = 'page_main.php';?>
                             <tr>
                               <td>11-20</td>
                               <td>
-                                <div class="progress progress-xs">
+                                <div class="progress progress-xs progress-striped active">
                                   <div class="progress-bar progress-bar-yellow" style="width: 70%"></div>
                                 </div>
                               </td>
@@ -333,6 +256,63 @@ $MasterPage = 'page_main.php';?>
               </div><!-- /.box -->
           </div><!-- /.row -->      
         </section><!-- /.content -->
+
+           <script src="<?php echo 'http://',$_SERVER['SERVER_NAME'],'/',APP_WEBROOT;?>/_plugins/chartjs/Chart.js" type="text/javascript"></script>
+                          <script src="<?php echo 'http://',$_SERVER['SERVER_NAME'],'/',APP_WEBROOT;?>/_plugins/chartjs/Chart.min.js" type="text/javascript"></script>
+                          <script>
+                              //-------------
+                              //- PIE CHART -
+                              //-------------
+                              // Get context with jQuery - using jQuery's .get() method.
+                              var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+                              var pieChart = new Chart(pieChartCanvas);
+                              var PieData = [
+                                {
+                                  value: 700,
+                                  color: "#f56954",
+                                  highlight: "#f56954",
+                                  label: "ชาย"
+                                },
+                                {
+                                  value: 500,
+                                  color: "#00a65a",
+                                  highlight: "#00a65a",
+                                  label: "หญิง"
+                                },
+                              ];
+                              var pieOptions = {
+                                //Boolean - Whether we should show a stroke on each segment
+                                segmentShowStroke: true,
+                                //String - The colour of each segment stroke
+                                segmentStrokeColor: "#fff",
+                                //Number - The width of each segment stroke
+                                segmentStrokeWidth: 1,
+                                //Number - The percentage of the chart that we cut out of the middle
+                                percentageInnerCutout: 50, // This is 0 for Pie charts
+                                //Number - Amount of animation steps
+                                animationSteps: 100,
+                                //String - Animation easing effect
+                                animationEasing: "easeOutBounce",
+                                //Boolean - Whether we animate the rotation of the Doughnut
+                                animateRotate: true,
+                                //Boolean - Whether we animate scaling the Doughnut from the centre
+                                animateScale: false,
+                                //Boolean - whether to make the chart responsive to window resizing
+                                responsive: true,
+                                // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+                                maintainAspectRatio: false,
+                                //String - A legend template
+                                legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+                                //String - A tooltip template
+                                tooltipTemplate: "<%=label%> <%=value %>  คน"
+                              };
+                              //Create pie or douhnut chart
+                              // You can switch between pie and douhnut using the method below.  
+                              pieChart.Doughnut(PieData, pieOptions);
+                              //-----------------
+                              //- END PIE CHART -
+                              //-----------------                          
+                          </script>
 
 
 <?php eb();?>
